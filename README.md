967211cd371f885e6b5300e4d64e7acf2ae9151f
Mohamad Azri Azhar <m.azri.azhar@gmail.com>
2017-06-11 18:13:17 +0800

    Sandbox initial project structure.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Sandbox
+Sandbox is a personal workspace to try out new ideas and store reusable Java code snippets. Sandbox components are not necessarily be maintained, or used in actual production.
---
+++ src/main/java/com/azriazhar/common/utilities/Observable.java
@@ -0,0 +1,71 @@
+package com.azriazhar.common.utilities;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.function.Consumer;
+
+/**
+ * A simple implementation of observer pattern without boilerplate code. A
+ * listener is executed in a single worker thread operating off an unbounded
+ * queue, with a timeout value to cancel the execution. This is to prevent from
+ * one listener from blocking other listeners on the list. Listeners are stored
+ * in a "snapshot" style thread-safe array.
+ *
+ * @author Mohamad Azri Azhar
+ *
+ * @param <T>
+ *            the type of object to be passed to observers/listeners upon
+ *            notification (the event source).
+ */
+public class Observable<T> {
+	private volatile long listenerExecutionTimeout = 0;
+	private List<Consumer<T>> listeners = new CopyOnWriteArrayList<>();
+	private ExecutorService executor = Executors.newSingleThreadExecutor();
+
+	public void addListener(final Consumer<T> consumer) {
+		if (consumer != null && !listeners.contains(consumer)) {
+			listeners.add(consumer);
+		}
+	}
+
+	public void removeListener(final Consumer<T> consumer) {
+		if (null != consumer) {
+			listeners.remove(consumer);
+		}
+	}
+
+	public long getEventExecutionTimeout() {
+		return listenerExecutionTimeout;
+	}
+
+	public void setEventExecutionTimeout(long listenerExecutionTimeout) {
+		this.listenerExecutionTimeout = listenerExecutionTimeout;
+	}
+
+	public void notifyListeners(final T event) {
+		for (final Consumer<T> consumer : listeners) {
+			Future<Void> future = null;
+
+			try {
+				future = executor.submit(() -> {
+					consumer.accept(event);
+					return null;
+				});
+
+				future.get(listenerExecutionTimeout, TimeUnit.SECONDS);
+			} catch (InterruptedException | ExecutionException | TimeoutException e) {
+				e.printStackTrace();
+			} finally {
+				if (listenerExecutionTimeout > 0 && future != null) {
+					future.cancel(true);
+				}
+			}
+		}
+	}
+}
