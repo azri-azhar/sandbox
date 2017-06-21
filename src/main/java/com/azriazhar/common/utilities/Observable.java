464647c7fafa4d8157ad440ae1afed1987b398fd
Mohamad Azri Azhar <m.azri.azhar@gmail.com>
2017-06-22 01:43:22 +0800

    Change source code style to Google Java Style.

--- src/main/java/com/azriazhar/common/utilities/Observable.java
+++ src/main/java/com/azriazhar/common/utilities/Observable.java
@@ -11,61 +11,59 @@ import java.util.concurrent.TimeoutException;
 import java.util.function.Consumer;
 
 /**
- * A simple implementation of observer pattern without boilerplate code. A
- * listener is executed in a single worker thread operating off an unbounded
- * queue, with a timeout value to cancel the execution. This is to prevent from
- * one listener from blocking other listeners on the list. Listeners are stored
- * in a "snapshot" style thread-safe array.
+ * A simple implementation of observer pattern without boilerplate code. A listener is executed in a
+ * single worker thread operating off an unbounded queue, with a timeout value to cancel the
+ * execution. This is to prevent from one listener from blocking other listeners on the list.
+ * Listeners are stored in a "snapshot" style thread-safe array.
  *
  * @author Mohamad Azri Azhar
  *
- * @param <T>
- *            the type of object to be passed to observers/listeners upon
- *            notification (the event source).
+ * @param <T> the type of object to be passed to observers/listeners upon notification (the event
+ *        source).
  */
 public class Observable<T> {
-	private volatile long listenerExecutionTimeout = 0;
-	private List<Consumer<T>> listeners = new CopyOnWriteArrayList<>();
-	private ExecutorService executor = Executors.newSingleThreadExecutor();
+  private volatile long listenerExecutionTimeout = 0;
+  private List<Consumer<T>> listeners = new CopyOnWriteArrayList<>();
+  private ExecutorService executor = Executors.newSingleThreadExecutor();
 
-	public void addListener(final Consumer<T> consumer) {
-		if (consumer != null && !listeners.contains(consumer)) {
-			listeners.add(consumer);
-		}
-	}
+  public void addListener(final Consumer<T> consumer) {
+    if (consumer != null && !listeners.contains(consumer)) {
+      listeners.add(consumer);
+    }
+  }
 
-	public void removeListener(final Consumer<T> consumer) {
-		if (consumer != null) {
-			listeners.remove(consumer);
-		}
-	}
+  public void removeListener(final Consumer<T> consumer) {
+    if (consumer != null) {
+      listeners.remove(consumer);
+    }
+  }
 
-	public long getEventExecutionTimeout() {
-		return listenerExecutionTimeout;
-	}
+  public long getEventExecutionTimeout() {
+    return listenerExecutionTimeout;
+  }
 
-	public void setEventExecutionTimeout(long listenerExecutionTimeout) {
-		this.listenerExecutionTimeout = listenerExecutionTimeout;
-	}
+  public void setEventExecutionTimeout(long listenerExecutionTimeout) {
+    this.listenerExecutionTimeout = listenerExecutionTimeout;
+  }
 
-	public void notifyListeners(final T event) {
-		for (final Consumer<T> consumer : listeners) {
-			Future<Void> future = null;
+  public void notifyListeners(final T event) {
+    for (final Consumer<T> consumer : listeners) {
+      Future<Void> future = null;
 
-			try {
-				future = executor.submit(() -> {
-					consumer.accept(event);
-					return null;
-				});
+      try {
+        future = executor.submit(() -> {
+          consumer.accept(event);
+          return null;
+        });
 
-				future.get(listenerExecutionTimeout, TimeUnit.SECONDS);
-			} catch (InterruptedException | ExecutionException | TimeoutException e) {
-				// Unrecoverable exception, ignore.
-			} finally {
-				if (listenerExecutionTimeout > 0 && future != null) {
-					future.cancel(true);
-				}
-			}
-		}
-	}
+        future.get(listenerExecutionTimeout, TimeUnit.SECONDS);
+      } catch (InterruptedException | ExecutionException | TimeoutException e) {
+        // Unrecoverable exception, ignore.
+      } finally {
+        if (listenerExecutionTimeout > 0 && future != null) {
+          future.cancel(true);
+        }
+      }
+    }
+  }
 }
